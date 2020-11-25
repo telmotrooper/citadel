424187c89b795e81384bdfeafb4458325581e233
Telmo Silva <tvsilva@tech6.com.br>
2020-11-25 10:51:55 -0300

    Added base example for GUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use std::env::args;
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("Citadel");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    let button = gtk::Button::with_label("OK");
+
+    window.add(&button);
+
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        gtk::Application::new(Some("com.github.telmotrooper.citadel"), Default::default())
+            .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
 }
