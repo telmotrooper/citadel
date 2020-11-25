266558b2edea4f712800c8da7b72beb1b322b1d3
Telmo Silva <tvsilva@tech6.com.br>
2020-11-25 11:03:13 -0300

    Added base example using Glade file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
+extern crate gio;
+extern crate glib;
+extern crate gtk;
+
 use gio::prelude::*;
+use glib::clone;
 use gtk::prelude::*;
+
+use gtk::{ApplicationWindow, Builder, Button, MessageDialog};
 
 use std::env::args;
 
 fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
+    let glade_src = include_str!("gui.glade");
+    let builder = Builder::from_string(glade_src);
 
-    window.set_title("Citadel");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(350, 70);
+    let window: ApplicationWindow = builder.get_object("window1").expect("Couldn't get window1");
+    window.set_application(Some(application));
+    let bigbutton: Button = builder.get_object("button1").expect("Couldn't get button1");
+    let dialog: MessageDialog = builder
+        .get_object("messagedialog1")
+        .expect("Couldn't get messagedialog1");
 
-    let button = gtk::Button::with_label("OK");
+    dialog.connect_delete_event(|dialog, _| {
+        dialog.hide();
+        gtk::Inhibit(true)
+    });
 
-    window.add(&button);
-
+    bigbutton.connect_clicked(clone!(@weak dialog => move |_| dialog.show_all()));
     window.show_all();
 }
 
 fn main() {
-    let application =
-        gtk::Application::new(Some("com.github.telmotrooper.citadel"), Default::default())
-            .expect("Initialization failed...");
+    let application = gtk::Application::new(
+        Some("com.github.telmotrooper.citadel"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
 
     application.connect_activate(|app| {
         build_ui(app);
