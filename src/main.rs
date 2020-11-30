1027f9f7afc79d3b1598ca9821624cbd609ea8e2
Telmo Silva <tvsilva@tech6.com.br>
2020-11-30 11:02:34 -0300

    Begun work on GUI

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ extern crate glib;
 extern crate gtk;
 
 use gio::prelude::*;
-use glib::clone;
 use gtk::prelude::*;
 
-use gtk::{ApplicationWindow, Builder, Button, MessageDialog};
+use gtk::{Window, Builder};
 
 use std::env::args;
 
@@ -14,19 +13,8 @@ fn build_ui(application: &gtk::Application) {
     let glade_src = include_str!("gui.glade");
     let builder = Builder::from_string(glade_src);
 
-    let window: ApplicationWindow = builder.get_object("window1").expect("Couldn't get window1");
+    let window: Window = builder.get_object("main_window").expect("Couldn't get main_window");
     window.set_application(Some(application));
-    let bigbutton: Button = builder.get_object("button1").expect("Couldn't get button1");
-    let dialog: MessageDialog = builder
-        .get_object("messagedialog1")
-        .expect("Couldn't get messagedialog1");
-
-    dialog.connect_delete_event(|dialog, _| {
-        dialog.hide();
-        gtk::Inhibit(true)
-    });
-
-    bigbutton.connect_clicked(clone!(@weak dialog => move |_| dialog.show_all()));
     window.show_all();
 }
 
