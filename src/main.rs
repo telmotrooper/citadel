4b86f7f9bf00783bf826c12f20a1043f9f08243a
Telmo Silva <tvsilva@tech6.com.br>
2020-12-01 12:28:59 -0300

    Connected event to open about dialog

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate glib;
 extern crate gtk;
 
 use gio::prelude::*;
+use glib::clone;
 use gtk::prelude::*;
 
-use gtk::{Window, Builder};
+use gtk::{AboutDialog, Window, Builder};
 
 use std::env::args;
 
@@ -13,7 +14,21 @@ fn build_ui(application: &gtk::Application) {
     let glade_src = include_str!("gui.glade");
     let builder = Builder::from_string(glade_src);
 
-    let window: Window = builder.get_object("main_window").expect("Couldn't get main_window");
+    let window: Window = builder.get_object("main_window").expect("Couldn't get \"main_window\".");
+
+    let about_dialog: AboutDialog = builder.get_object("about_dialog").expect("Couldn't get \"about_dialog\".");
+
+    builder.connect_signals(move |_, handler_name| {
+        if handler_name == "show_about_dialog" {
+            Box::new(clone!(@weak about_dialog => @default-return None, move |_| {
+                about_dialog.show_all();
+                None
+            }))
+        } else {
+            panic!("Unknown handler name {}", handler_name)
+        }
+    });
+
     window.set_application(Some(application));
     window.show_all();
 }
